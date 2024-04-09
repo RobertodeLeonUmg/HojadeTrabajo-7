50525ceb6ae9f70977db97a24dfb2a3fe38130e7
RobertodeLeonUmg <157396802+RobertodeLeonUmg@users.noreply.github.com>
2024-04-08 19:29:49 -0600

    HojadeTrabajo#7.cpp

--- HojadeTrabajo#7.cpp
+++ HojadeTrabajo#7.cpp
@@ -1,4 +1,4 @@
-//Roberto Carlos de León Gramajo
+//Roberto Carlos de LeÃ³n Gramajo
 //5090-23-1762
 #include <iostream>
 #include <fstream>
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-// Definición de la clase Persona
+// DefiniciÃ³n de la clase Persona
 class Persona {
 protected:
     struct DatosPersona {
@@ -20,17 +20,18 @@ protected:
 
 public:
     void obtenerDatosPersona() {
+    	cout << "\nDatos Personales\n";
         cout << "Ingrese el nombre: ";
         cin >> datos.nombre;
         cout << "Ingrese el apellido: ";
         cin >> datos.apellido;
         cout << "Ingrese la edad: ";
         cin >> datos.edad;
-        cout << "Ingrese el día de cumpleaños: ";
+        cout << "Ingrese el dia de cumpleanos: ";
         cin >> datos.diaCumple;
-        cout << "Ingrese el mes de cumpleaños: ";
+        cout << "Ingrese el mes de cumpleanos: ";
         cin >> datos.mesCumple;
-        cout << "¿Tiene trabajo? (1: Sí / 0: No): ";
+        cout << "Â¿Tiene trabajo? (1: Si / 0: No): ";
         cin >> datos.tieneTrabajo;
     }
 
@@ -38,17 +39,17 @@ public:
         archivo << "Nombre: " << datos.nombre << endl;
         archivo << "Apellido: " << datos.apellido << endl;
         archivo << "Edad: " << datos.edad << endl;
-        archivo << "Cumpleaños: " << datos.diaCumple << "/" << datos.mesCumple << endl;
-        archivo << "Tiene trabajo: " << (datos.tieneTrabajo ? "Sí" : "No") << endl;
+        archivo << "CumpleaÃ±os: " << datos.diaCumple << "/" << datos.mesCumple << endl;
+        archivo << "Tiene trabajo: " << (datos.tieneTrabajo ? "SÃ­" : "No") << endl;
     }
 
-    // Método para obtener el nombre de la persona
+    // MÃ©todo para obtener el nombre de la persona
     string obtenerNombre() {
         return datos.nombre;
     }
 };
 
-// Definición de la clase Estudiante que hereda de Persona
+// DefiniciÃ³n de la clase Estudiante que hereda de Persona
 class Estudiante : public Persona {
 private:
     string numeroCarnet;
@@ -59,31 +60,32 @@ private:
 public:
     void obtenerDatosEstudiante() {
         obtenerDatosPersona();
-        cout << "Ingrese el número de carnet de estudiante: 5090-23-";
+        cout << "\nDatos Estudiante\n";
+        cout << "Ingrese el numero de carnet de estudiante: 5090-23-";
         cin >> numeroCarnet;
         cout << "Ingrese el semestre: ";
         cin >> semestre;
         cout << "Ingrese el nombre de la carrera universitaria: Ingenieria en ";
         cin >> carreraUniversitaria;
-        cout << "1ra. Lunes, Miércoles y Viernes de 07:00 a 12:00, Martes y Jueves de 08:30 a 12:00\n";
-        cout << "2da. Lunes, Miércoles y Viernes de 07:00 a 12:00, Martes y Jueves de 07:00 a 10:00\n";
+        cout << "1ra. Lunes, Miercoles y Viernes de 07:00 a 12:00, Martes y Jueves de 08:30 a 12:00\n";
+        cout << "2da. Lunes, Miercoles y Viernes de 07:00 a 12:00, Martes y Jueves de 07:00 a 10:00\n";
         cout << "Elija un horario de clases (1 o 2): ";
         int opcion;
         cin >> opcion;
-        horarioClases = (opcion == 1) ? "Lunes, Miércoles y Viernes de 07:00 a 12:00, Martes y Jueves de 08:30 a 12:00"
-                                      : "Lunes, Miércoles y Viernes de 07:00 a 12:00, Martes y Jueves de 07:00 a 10:00";
+        horarioClases = (opcion == 1) ? "Lunes, Miercoles y Viernes de 07:00 a 12:00, Martes y Jueves de 08:30 a 12:00"
+                                      : "Lunes, Miercoles y Viernes de 07:00 a 12:00, Martes y Jueves de 07:00 a 10:00";
     }
 
     void guardarDatosEnArchivo(ofstream &archivo) {
         Persona::guardarDatosEnArchivo(archivo);
-        archivo << "Número de carnet: 5090-23-" << numeroCarnet << endl;
+        archivo << "NÃºmero de carnet: 5090-23-" << numeroCarnet << endl;
         archivo << "Semestre: " << semestre << endl;
         archivo << "Carrera universitaria: " << carreraUniversitaria << endl;
         archivo << "Horario de clases: " << horarioClases << endl;
     }
 };
 
-// Función para el algoritmo de ordenamiento de burbuja
+// FunciÃ³n para el algoritmo de ordenamiento de burbuja
 void ordenarNombres(string nombres[], int cantidad) {
     for (int i = 0; i < cantidad - 1; i++) {
         for (int j = 0; j < cantidad - i - 1; j++) {
@@ -136,4 +138,3 @@ int main() {
     archivo.close();
     return 0;
 }
-
